0491f4599d5c6ed872c057cfa69b2d656f2b79d8
Ashraful Islam <ashrafulislam.dev1@gmail.com>
2024-10-03 13:51:06 +0600

    Added verified icon dynamically

--- scripts/videos.js
+++ scripts/videos.js
@@ -23,13 +23,19 @@ const displayVideos = (videos) => {
   </figure>
   <div class="px-0 py-2 flex gap-2">
     <div>
-      <img class="w-10 h-10 rounded-full object-cover" src=${video.authors[0].profile_picture}/>
+      <img class="w-10 h-10 rounded-full object-cover" src=${
+        video.authors[0].profile_picture
+      }/>
     </div>
     <div>
         <h2 class="font-bold">${video.title}</h2>
         <div class="flex items-center gap-2">
             <p>${video.authors[0].profile_name}</p>
-            <img class="w-5 object-cover" src="https://img.icons8.com/?size=48&id=D9RtvkuOe31p&format=png">
+            ${
+              video.authors[0].verified
+                ? '<img class="w-5 object-cover" src="https://img.icons8.com/?size=48&id=D9RtvkuOe31p&format=png">'
+                : ""
+            }
         </div>
     </div>
   </div>
